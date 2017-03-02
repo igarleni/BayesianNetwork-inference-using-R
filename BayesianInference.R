297c804a2135422585e01e8c1d5b56214db0af5d
Ítalo Garleni Rodríguez <italogar6@hotmail.com>
2017-03-02 19:25:49 +0100

    Probability especification
    specifying probabilities to conditional distributions. There are different ways depending on parents number

--- BayesianInference.R
+++ BayesianInference.R
@@ -70,3 +70,43 @@ biocLite("Rgraphviz")
 graphviz.plot(dag1)
 #this plot function sort nodes by reducing arcs and nodes overlap
 
+
+################################
+## Probability especification ##
+################################
+
+##Establis variable domains
+A.st <- c("young", "adult", "old")
+S.st <- c("M", "F")
+E.st <- c("high", "uni")
+O.st <- c("emp", "self")
+R.st <- c("small", "big")
+T.st <- c("car", "train", "other")
+
+##The DAG have this factorized distribution:
+## P(A,S,E,O,R,T) = P(A)P(S)P(E|A,S)P(O|E)P(R|E)P(T|O,R)
+
+##Asign probabilities:
+# -to non-Parents variables, P(A) P(S)
+A.prob <- array(c(0.30, 0.50, 0.20), dim = 3,
+                dimnames = list (A = A.st))
+S.prob <- array(c(0.60, 0.40), dim = 2,
+                dimnames =  list(S = S.st))
+# -to mono-Parents variables, P(O|E) P(R|E)
+#  (can be done with matrix or array)
+O.prob <- array(c(0.96, 0.04, 0.92, 0.08), dim=c(2,2),
+                dimnames = list(O = O.st, E = E.st))
+R.prob <- matrix(c(0.25, 0.75, 0.20, 0.80), ncol = 2,
+                 dimnames = list(R = R.st, E = E.st))
+# -to multiple-Parents variables, P(E|A,S) (T|O,R)
+E.prob <- array(c(0.75, 0.25, 0.72, 0.28, 0.88, 0.12, 0.64, 
+                  0.36, 0.70, 0.30, 0.90, 0.10), dim=c(2, 3, 2), 
+                dimnames = list(E = E.st, A = A.st, S = S.st))
+T.prob <- array(c(0.48, 0.42, 0.10, 0.56, 0.36, 0.08, 0.58,
+                  0.24, 0.18, 0.70, 0.21, 0.09), dim=c(3, 2, 2),
+                dimnames = list(T = T.st, O = O.st, R = R.st))
+
+
+###############################
+## Bayesian Network creation ##
+###############################
